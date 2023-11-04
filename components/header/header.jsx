641af93b4ead1218c6bbd45720cc9d46dffec965
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-11-04 21:29:22 +0200

    header mobile & desktop

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
-import searchGlass from "../public/icon/searchGlass.svg"
-import Logo from "../public/img/logo.png"
-import regIcon from "../public/icon/person.svg"
-import basket from "../public/icon/backet.svg"
-import burger from "../public/icon/burger.svg"
+import searchGlass from "../../public/icon/searchGlass.svg"
+import Logo from "../../public/img/logo.png"
+import regIcon from "../../public/icon/person.svg"
+import basket from "../../public/icon/backet.svg"
+import burger from "../../public/icon/burger.svg"
 
 export const  Header =() => {
     return (
@@ -11,18 +11,21 @@ export const  Header =() => {
             <div className="bg-[#D0C3BB] h-8 w-full"></div>
             <div className="flex">
                 <div className="flex">
-                    <div className="relative  hidden md:block lg:visible">
-                        <input type="serch" placeholder="пошук" className="p-3 w-[219px] h-[37px] " />
-                        <button className="absolute top-2 left-2">
+                    <div className="relative  hidden md:block lg:visible ml-[96px] lg:pt-[14px]">
+                        <input type="serch" placeholder="пошук" className="pl-10 w-[219px] h-[37px] " />
+                        <button className="absolute top-6 left-3">
                             <Image                            
                             src={searchGlass}
-                            width={15}
-                            height={15}
+                            width={20}
+                            height={20}
                             alt="searchGlass"
                             />
-                        </button>
+                        </button>                       
+                    </div>
 
-                        <button className="visible sm:visible md:invisible lg:invisible">
+                    {/* контейнер з бургер */}
+                    <div>
+                    <button className="visible md:hidden lg:hidden ml-5 pt-4 pb-4">
                             <Image
                             src={burger}
                             width={15}
@@ -31,8 +34,9 @@ export const  Header =() => {
                             />
                         </button>
                     </div>
+
                     {/* контейнер з лого */}
-                    <div>
+                    <div className="ml-16 pt-[10px] pb-[10px] lg:ml-[270px]">
                         <Image
                         className="w-[114px] h-5 lg:w-[249px] lg:h-[45px]"
                         src={Logo}                       
@@ -40,14 +44,15 @@ export const  Header =() => {
                         />
                     </div>
                      {/* контейнер з іконками */}
-                     <div className="flex">
+                     <div className="flex ml-[148px] lg:ml-[500px]">
                         <Image
+                        className="hidden md:block lg:visible lg:mr-3"
                         src={regIcon}
                         width={19}
                         height={21}
                         alt="regIcon"
                          />
-                          <Image
+                          <Image                          
                         src={basket}
                         width={19}
                         height={24}
