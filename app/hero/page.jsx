e357b0a4c816d8c91d0f051fe1b6dfa4e47f772c
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-11-04 22:27:24 +0200

    hero mobile resistant

--- app/hero/page.jsx
+++ app/hero/page.jsx
@@ -1,15 +1,29 @@
 import Image from "next/image"
-import HeroMob from "../../public/img/hero/heroMob-1x.png"
+import HeroMob from "../../public/img/hero/heroMob-3x.png"
 import HeroDT from "../../public/img/hero/heroDT-3x.jpg"
 
 const Hero = () => {
     return (
-        <div className=" visible md:hidden ld:hedden sm:w-full h-[521px]">
+        <>
+             {/* // logo for mobile */}
+        <div className=" visible md:hidden lg:hidden w-screen h-[521px]">
             <Image 
             src={HeroMob}
             alt="HeroMob"
             />
         </div>
+
+        {/* // logo for DeskTop */}
+        <div className="hidden lg:block w-screen h-[525px] ">
+        <Image 
+            src={HeroDT}
+            alt="HeroMob"
+            />
+        </div>        
+        </>
+   
+
+
     )
 }
 export default Hero
