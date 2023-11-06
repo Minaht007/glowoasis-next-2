1b3b191ec4e2b04365ece1bcdbeda43f344e0506
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-11-06 12:33:56 +0200

    smiper.module.scss

--- app/hero/page.jsx
+++ app/hero/page.jsx
@@ -1,25 +1,11 @@
 import Image from "next/image"
-import HeroMob from "../../public/img/hero/heroMob-3x.png"
+import SwiperHero from "../../components/Swiper/SwiperHero"
 import HeroDT from "../../public/img/hero/heroDT-3x.jpg"
 
 const Hero = () => {
     return (
         <>
-             {/* // logo for mobile */}
-        <div className=" visible md:hidden lg:hidden w-screen h-[521px]">
-            <Image 
-            src={HeroMob}
-            alt="HeroMob"
-            />
-        </div>
-
-        {/* // logo for DeskTop */}
-        <div className="hidden lg:block w-screen h-[525px] ">
-        <Image 
-            src={HeroDT}
-            alt="HeroMob"
-            />
-        </div>        
+        <SwiperHero />          
         </>
    
 
---
+++ components/Swiper/SwiperHero.jsx
@@ -0,0 +1,72 @@
+"use client"
+import Image from "next/image"
+import HeroMob from "../../public/img/hero/heroMob-3x.png"
+import HeroMob2 from "../../public/img/hero/heroMob-1x.png"
+import HeroDT1 from "../../public/img/hero/heroDT-3x.jpg"
+import HeroDT2 from "../../public/img/hero/heroDT-3x-2.jpg"
+
+import styles from "./swiper.module.scss"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+export default function SwiperHero() {
+  return (
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {/* slider containers */}
+        <SwiperSlide className={styles.heroMob} >
+        {/* className="sm:visible md:hidden lg:!hidden w-screen h-[521px]" */}
+        <Image 
+            src={HeroMob}
+            alt="HeroMob"
+            />
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.heroMob} >
+        {/* className="sm:visible md:hidden lg:!hidden w-screen h-[521px]" */}
+        <Image 
+            src={HeroMob2}
+            alt="HeroMob"
+            />
+        </SwiperSlide>
+
+        {/* For DeskTop */}
+        <SwiperSlide className={styles.heroDT} >
+        {/* className="sm:hidden lg:block w-screen h-[525px]" */}
+        <Image 
+            src={HeroDT1}
+            alt="HeroDT1"
+            />
+        </SwiperSlide>
+        <SwiperSlide className={styles.heroDT} >
+        {/* className="sm:hidden lg:block w-screen h-[525px]" */}
+        <Image 
+            src={HeroDT2}
+            alt="HeroDT2"
+            />
+        </SwiperSlide>
+        
+        {/* <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
+      </Swiper>
+    </>
+  );
+}
