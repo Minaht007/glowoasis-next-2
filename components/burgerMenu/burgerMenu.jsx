144a04811232734479d2598ab9f2384104179ae7
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-12-14 09:00:56 +0200

    123

--- components/burgerMenu/burgerMenu.jsx
+++ components/burgerMenu/burgerMenu.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import styles from "./burgerMenu.module.scss"
+import fontstyles from "../fonts/fonts.module.scss"
 
 
 
@@ -9,10 +10,13 @@ const BurgerMenu = ({ links, isActive }) => {
   return (
     <ul className={`${styles.burgerMenu} ${isActive ? styles.active : ''}`}>
         {isActive && links.map((item) => (
-        <li key={item.name}>
+        <li key={item.name} className={`${fontstyles.istokWebTitle } py-4 text-textColor`}>
           <Link href={item.link}>{item.name}</Link>
         </li>
       ))}
+      <li>
+        <span>profile</span>
+      </li>
     </ul>
   );  
 };
