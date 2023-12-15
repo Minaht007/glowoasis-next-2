d6be384c13f6183af0c2d41cc5430769356db058
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-12-15 15:56:03 +0200

    signUp form

--- components/burgerMenu/burgerMenu.jsx
+++ components/burgerMenu/burgerMenu.jsx
@@ -1,25 +1,50 @@
-"use client";
 import Link from "next/link";
-import styles from "./burgerMenu.module.scss"
-import fontstyles from "../fonts/fonts.module.scss"
+import styles from "./burgerMenu.module.scss";
+import fontstyles from "../fonts/fonts.module.scss";
+import Image from "next/image";
+import signicon from "../../public/icon/person.svg";
+import SignUp from "../signIn/signIn";
+import React, { useState } from "react";
 
+const BurgerMenu = ({ links, isActive }) => {
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [previousLinks, setPreviousLinks] = useState(links);
 
-
-const BurgerMenu = ({ links, isActive }) => {     
+  const handleSignUpClick = () => {
+    setShowSignUp(!showSignUp);
+    if (showSignUp) {
+      setPreviousLinks(links);
+    } else {
+      setPreviousLinks([]);
+    }
+  };
 
   return (
-    <ul className={`${styles.burgerMenu} ${isActive ? styles.active : ''}`}>
-        {isActive && links.map((item) => (
-        <li key={item.name} className={`${fontstyles.istokWebTitle } py-4 text-textColor`}>
-          <Link href={item.link}>{item.name}</Link>
+    <>
+      <ul className={`${styles.burgerMenu} ${isActive ? styles.active : ""}`}>
+        {isActive &&
+          previousLinks.map((item) => (
+            <li
+              key={item.name}
+              className={`${fontstyles.istokWebTitle} py-4 text-textColor`}
+            >
+              <Link href={item.link}>{item.name}</Link>
+            </li>
+          ))}
+
+        <li className="flex flex-row relative" onClick={handleSignUpClick}>
+          <button>
+            <Image src={signicon} alt="signicon" width={12} height={14} />
+            <p className="absolute top-[-14px] left-5 text-start">
+              мій <br /> профіль
+            </p>
+          </button>
         </li>
-      ))}
-      <li>
-        <span>profile</span>
-      </li>
-    </ul>
-  );  
+
+        {showSignUp && <SignUp />}
+      </ul>
+    </>
+  );
 };
+
 export default BurgerMenu;
-
-
