a138e058be4af42b62ee814c7531d5a7fe3a8e76
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-01-26 23:53:40 +0200

    12344567

--- components/contactUs/ContactUsComponent.jsx
+++ components/contactUs/ContactUsComponent.jsx
@@ -12,7 +12,7 @@ export const ContactUs = () => {
          height={531}
          className="mr-14"/> 
          <div className="flex flex-col w-[553px] text-start">
-            <h2 className="text-7xl font-bold mb-5 text-start"> зв'яжися з нами</h2>
+            <h2 className="text-7xl font-bold mb-5 text-start"> зв(&apos;)яжися з нами</h2>
             <p className="text-base mb-5 text-start">Ми тут, щоб допомогти вам досягти найздоровішої шкіри! Будь ласка, залиште свої запитання та коментарі нижче, і ми відповімо на них якнайшвидше.</p>
             <p className="text-base mb-5 text-start">Перевірте нашу сторінку з найчастішими запитаннями FAQ для інформації щодо доставки та повернень, продуктів та іншої інформації!</p>
             <p className="text-base text-start">Для всіх запитань щодо обслуговування клієнтів, будь ласка, надсилайте листи на адресу glowoasis@gmail.com</p>
